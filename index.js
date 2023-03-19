15aa4988ac0db03c7e9f186546e004ee0b2b4471
Tejas <tejassonar227@gmail.com>
2023-03-20 00:59:33 +0530

    added an empty get route

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ mongoose
 const app = express();
 
 app.use("/api/items", itemRoutes);
-
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the Order Booking app!",
+  });
+});
 // require("./app/routes/organizer.routes.js")(app);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
